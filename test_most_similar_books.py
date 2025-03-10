5f2ca3d4f1f6d3b539c6dbdb3971ab8cb7e5e8d2
CallumCamazzola <ccamazzo@uwaterloo.ca>
2025-03-09 21:54:15 -0400

    Update test_most_similar_books.py

--- test_most_similar_books.py
+++ test_most_similar_books.py
@@ -2,12 +2,9 @@ from most_similar_books import main
 import os
 
 def test_most_similar_books_main():
-    # the correct answers as computed by the instructor, do not edit
-    # the most-similar-books-correct-answer.txt file or you will mess up
-    # this test!
     correct_file = open("most-similar-books-correct-answer.txt", "r")
 
-    main() # when main runs, it should write out most-similar-books.txt
+    main() # when main runs, it writes out most-similar-books.txt
     actual_output_filename = "most-similar-books.txt"
     actual_file = open(actual_output_filename, "r")
 
